20148c9a5ced53a97507bc55666bb4673048e13a
Mehedi390 <mh0816150@gmail.com>
2021-09-27 12:28:37 -0700

    show nwes form news nwes api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Card from './Conponents/Card/Card';
+import { useState, useEffect } from 'react';
+import { ButtonGroup, Button, Spinner, Row } from 'react-bootstrap';
+// const [items, setItems] = useState([])
+
 
 function App() {
+  const [news, setNews] = useState([])
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-08-27&sortBy=publishedAt&apiKey=60ebc7ffe93a4ef6a218dc51b707a5d3')
+      .then(res => res.json())
+      .then(data => setNews(data.articles))
+  }
+
+    , [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <Row className="g-4" xs={1} md={2}>
+          {
+            news.map(nw => <Card news={nw}></Card>)
+          }
+        </Row>
+
+
+
+      </div>
     </div>
   );
 }
